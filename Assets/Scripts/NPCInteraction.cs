701f29974c1802041a6af718aa02c6b8865d7c83
Minho Park <alsgh4442@naver.com>
2024-05-20 15:59:20 +0900

    NPC역할부여, 애니메이션 적용

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class NPCInteraction : MonoBehaviour
 {
     public PopupController popupController; // Inspector에서 할당할 PopupController 컴포넌트
+<<<<<<< Updated upstream
+=======
+    public GameObject Player;
+    private Animator animator; // Animator 컴포넌트
+>>>>>>> Stashed changes
 
     private bool isPlayerInRange = false;
+
+    void Start()
+    {
+        // Animator 컴포넌트 가져오기
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -14,12 +25,27 @@ public class NPCInteraction : MonoBehaviour
             // 팝업창의 활성화 상태를 토글
             if (popupController.popupPanel.activeSelf)
             {
+<<<<<<< Updated upstream
                 popupController.ClosePopup();
             }
             else
             {
                 popupController.OpenPopup();
             }
+=======
+                ClosePopup();
+            }
+            else
+            {
+                OpenPopup();
+            }
+        }
+
+        // 대화창이 열린 상태에서 'K' 키를 누르면
+        if (popupController.popupPanel.activeSelf && Input.GetKeyDown(KeyCode.K))
+        {
+            animator.SetTrigger("isSurprised");
+>>>>>>> Stashed changes
         }
     }
 
@@ -38,7 +64,27 @@ public class NPCInteraction : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerInRange = false;
-            popupController.ClosePopup(); // 플레이어가 범위를 벗어나면 팝업창 비활성화
+            ClosePopup(); // 플레이어가 범위를 벗어나면 팝업창 비활성화
         }
+    }
+
+    // 대화창을 여는 메서드
+    private void OpenPopup()
+    {
+        popupController.NPC = gameObject;
+        popupController.Player = Player;
+        popupController.OpenPopup();
+        Player.SetActive(false); // 대화 시작 시 플레이어 비활성화
+        Cursor.lockState = CursorLockMode.None; // 마우스 커서 잠금 해제
+        Cursor.visible = true; // 마우스 커서 보이기
+    }
+
+    // 대화창을 닫는 메서드
+    private void ClosePopup()
+    {
+        popupController.ClosePopup();
+        Player.SetActive(true); // 대화 종료 시 플레이어 활성화
+        Cursor.lockState = CursorLockMode.Locked; // 마우스 커서 잠금
+        Cursor.visible = false; // 마우스 커서 숨기기
     }
 }
