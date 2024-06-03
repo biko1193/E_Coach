c01bf04799dabdb77961cfd472a721c494908c02
Minho Park <alsgh4442@naver.com>
2024-06-03 16:12:25 +0900

    플레이어 미니맵 마커 버그 수정

--- Assets/MiniMap/FixedMiniMapMarker.cs
+++ Assets/MiniMap/FixedMiniMapMarker.cs
@@ -1,18 +1,17 @@
 using UnityEngine;
 
-public class FixedMinimapMarker : MonoBehaviour
+public class FixedMiniMapMarker : MonoBehaviour
 {
-    private Vector3 initialLocalPosition;
-
-    void Start()
-    {
-        // 초기 로컬 위치를 저장합니다.
-        initialLocalPosition = transform.localPosition;
-    }
+    public Transform player; // 플레이어의 Transform을 참조하기 위해 public으로 선언
 
     void LateUpdate()
     {
-        // 로컬 위치를 초기 위치로 고정시킵니다.
-        transform.localPosition = initialLocalPosition;
+        // 마커의 위치를 플레이어의 위치로 설정, y 축은 고정된 높이로 설정
+        Vector3 newPosition = player.position;
+        newPosition.y += 200; // 마커를 고정할 높이
+        transform.position = newPosition;
+
+        // 플레이어의 회전값을 사용하지 않도록 마커의 회전값을 고정
+        transform.rotation = Quaternion.Euler(90, 0, 0); // y축을 기준으로 회전하지 않게 고정
     }
 }
